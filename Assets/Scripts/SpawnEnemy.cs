0d2b132babd5b0d6d924aa920932c718571df0a3
MetaSoc <pk303187@student.polsl.pl>
2025-12-11 00:07:30 +0100

    cosmetic

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using Random = UnityEngine.Random;
 
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class SpawnEnemy : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class SpawnEnemy : MonoBehaviour
     [SerializeField] private GameObject bossPrefab;
 
     [SerializeField] private GameObject fireflyPrefab;
-    
+
     [Header("##  GAME MANAGER  ##")]
     private GameManager gameManager;
     [SerializeField] private AudioSource[] audioSource;
@@ -23,13 +24,13 @@ public class SpawnEnemy : MonoBehaviour
     [ShowOnly] public float spawnIntervalInit;
 
     private WaveNumberController waveController;
-    
+
     private float timer = 2;
 
     private float spawnPoint = 37;
     public InputActionAsset inputActions;
     private InputAction nextWaveAction;
-    
+
     private int waveNumber = 1;
     private int enemiesToSpawn;
     private int enemiesSpawned = 0;
@@ -49,8 +50,8 @@ public class SpawnEnemy : MonoBehaviour
         nextWaveAction = playerActionMap.FindAction("Start Wave");
         nextWaveAction.Enable();
     }
-
-
+    
+    
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
@@ -66,7 +67,7 @@ public class SpawnEnemy : MonoBehaviour
         Debug.Log("Wave " + waveNumber + " started");
         StartCoroutine(gameManager.Lightning());
     }
-    
+
 
 
 
@@ -88,15 +89,13 @@ public class SpawnEnemy : MonoBehaviour
             }
             return;
         }
-
+        
         if (enemiesSpawned < enemiesToSpawn)
         {
 
 
             if (timer < spawnInterval)
-            {
                 timer += Time.deltaTime;
-            }
             else
             {
                 spawnInterval = Random.Range(spawnIntervalInit - spawnIntervalOffset, spawnIntervalInit + spawnIntervalOffset);
@@ -105,39 +104,42 @@ public class SpawnEnemy : MonoBehaviour
                 enemiesAlive++;
                 timer = 0f;
             }
-            if(waveNumber % 5 == 0 && waveNumber != 0 && enemiesSpawned == enemiesToSpawn)
+
+            // SPAWN BOSS EVERY 5 WAVES
+            if (waveNumber % 5 == 0 && waveNumber != 0 && enemiesSpawned == enemiesToSpawn)
             {
-                for (int i = 0; i < waveNumber/5; i++)
+                for (int i = 0; i < waveNumber / 5; i++)
                 {
                     SpawnBossInstance();
                     enemiesSpawned++;
                 }
-                    
+
             }
         }
 
         // ALL ENEMIES SPAWNED AND DEFEATED
         if (enemiesSpawned == enemiesToSpawn && enemiesAlive == 0)
         {
-            
+
             // NEXT WAVE
             waitingForNextWave = true;
             waveNumber++;
             enemiesToSpawn += 3;
-            
-            waveController.ChangeWave(waveNumber-1);
+
+            waveController.ChangeWave(waveNumber - 1);
+            Debug.Log("Wave ended");
         }
     }
 
-
+    
     public void OnEnemyKilled()
     {
         if (Random.Range(0, 100) < 5)
         {
             if (Random.Range(0, 10) % 2 == 1)
-            Instantiate(fireflyPrefab, new Vector2(-spawnPoint, Random.Range(3f, 5f)), transform.rotation).transform.parent = transform;
+                Instantiate(fireflyPrefab, new Vector2(-spawnPoint, Random.Range(3f, 5f)), transform.rotation).transform.parent = transform;
             else
-            Instantiate(fireflyPrefab, new Vector2(spawnPoint, Random.Range(3f, 5f)), transform.rotation).transform.parent = transform;
+                Instantiate(fireflyPrefab, new Vector2(spawnPoint, Random.Range(3f, 5f)), transform.rotation).transform.parent = transform;
         }
         enemiesAlive--;
     }
@@ -148,40 +150,43 @@ public class SpawnEnemy : MonoBehaviour
         timer = 0f;
     }
 
-    
 
 
 
-    
+
+
     void SpawnEnemyInstance()
     {
         // ODD NUMBERS - LEFT SPAWN
         if (Random.Range(0, 10) % 2 == 1)
             Instantiate(enemyPrefab, new Vector2(-spawnPoint, transform.position.y), transform.rotation).transform.parent = transform;
-        
+
         // EVEN NUMBERS - RIGHT SPAWN
         else
             Instantiate(enemyPrefab, new Vector2(spawnPoint, transform.position.y), transform.rotation).transform.parent = transform;
 
         // To spawn just one enemy
         // this.enabled = false;
+
+        playerCombat.ChangeEyesIntensity();
     }
-    
-    
+
+
     void SpawnBossInstance()
     {
         // ODD NUMBERS - LEFT SPAWN
         if (Random.Range(0, 10) % 2 == 1)
             Instantiate(bossPrefab, new Vector2(-spawnPoint, transform.position.y), transform.rotation).transform.parent = transform;
-        
+
         // EVEN NUMBERS - RIGHT SPAWN
         else
             Instantiate(bossPrefab, new Vector2(spawnPoint, transform.position.y), transform.rotation).transform.parent = transform;
-        
+
         audioSource[Random.Range(0, audioSource.Length)].Play();
         // To spawn just one boss
         // this.enabled = false;
 
         playerCombat.ChangeEyesIntensity();
     }
+
 }
