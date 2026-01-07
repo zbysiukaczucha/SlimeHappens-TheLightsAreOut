3767ffaa546be2141f5c4b7fea8510437d3a32b1
Byouts <mo300354@student.polsl.pl>
2026-01-08 00:14:30 +0100

    Added remembering which walls were destroyed

--- Assets/Scripts/WallDestroyTrigger.cs
+++ Assets/Scripts/WallDestroyTrigger.cs
@@ -14,10 +14,26 @@ public class WallDestroyTrigger : MonoBehaviour
     string prevObjective;
     bool enteredTrigger;
 
+    int wallNumber;
+
     private void Start()
     {
         objectiveText = GameObject.FindWithTag("Objective").GetComponent<TextMeshProUGUI>();
         enteredTrigger = false;
+        GameObject[] walls = GameObject.Find("WorldEventManager").GetComponent<WorldEventManager>().wallsToBeDestroyed;
+        for(int i = 0; i < walls.Length; i++)
+        {
+            if (walls[i] == gameObject)
+            {
+                wallNumber = i;
+            }
+        }
+        if( UltimateGameManager.Instance.destroyedWallNumbers.Contains(wallNumber))
+        {
+            gameObject.SetActive(false);
+            print(gameObject.name + " is in destroyed walls");
+        }
+
     }
 
     private void Update()
@@ -30,6 +46,8 @@ public class WallDestroyTrigger : MonoBehaviour
                 {
                     // Break the wall
                     stoneWall.SetActive(false);
+                    UltimateGameManager.Instance.destroyedWallNumbers.Add(wallNumber);
+                    print("Adding " + gameObject.name + " to destroyed walls");
                     StartCoroutine(ChangeScenes());
                 }
             }
